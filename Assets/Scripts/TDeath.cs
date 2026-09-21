4474ce5a041e97e870741537c90064a7c7e1544b
Stugane <stugane@gmail.com>
2026-09-21 09:43:44 +0300

    Update TDeath.cs

--- Assets/Scripts/TDeath.cs
+++ Assets/Scripts/TDeath.cs
@@ -11,16 +11,4 @@ public class TDeath : MonoBehaviour
         triggerdeath = other.GetComponent<BoxCollider>();
         Destroy(GameObject.FindWithTag("Player"));
     }
-
-    //// Start is called once before the first execution of Update after the MonoBehaviour is created
-    //void Start()
-    //{
-
-    //}
-
-    //// Update is called once per frame
-    //void Update()
-    //{
-
-    //}
 }
